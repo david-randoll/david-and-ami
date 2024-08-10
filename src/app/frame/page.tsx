d3fc5349fa8ec28e951748cd3095fdb41cabe2cb
d-randoll <51640895+d-randoll@users.noreply.github.com>
2024-08-10 18:49:01 -0400

    revert back the image

--- src/app/frame/page.tsx
+++ src/app/frame/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React from "react";
 
 const HelloAmisha = () => {
@@ -6,7 +5,7 @@ const HelloAmisha = () => {
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
             <div className="bg-white p-8 rounded-lg shadow-xl text-center border-4 border-gray-300">
                 <div className="relative inline-block">
-                    <Image src="/bubu-dudu.jpg" alt="Cute cats" className="mx-auto mb-4 w-36 h-36 rounded-full border-4 border-yellow-500" />
+                    <img src="/bubu-dudu.jpg" alt="Cute cats" className="mx-auto mb-4 w-36 h-36 rounded-full border-4 border-yellow-500" />
                 </div>
                 <div className="text-3xl font-bold text-gray-900 italic font-sans">David & Amisha</div>
             </div>
