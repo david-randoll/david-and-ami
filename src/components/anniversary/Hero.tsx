66580f59b6e3aaa54b3b099e22b308e9986c1967
d-randoll <51640895+d-randoll@users.noreply.github.com>
2024-09-21 15:09:20 -0400

    add sparkle to the landing page

--- src/components/anniversary/Hero.tsx
+++ src/components/anniversary/Hero.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { SparklesCore } from "../ui/sparkles";
 
 const Hero = () => {
     return (
@@ -8,6 +9,17 @@ const Hero = () => {
             <div className="text-center text-white bg-opacity-60 backdrop-blur-md p-8 rounded-lg">
                 <h1 className="text-5xl font-extrabold mb-4">Happy 1 Year Anniversary!</h1>
                 <p className="text-xl">A year of love, laughter, and endless memories.</p>
+            </div>
+            <div className="w-full absolute inset-0 h-screen">
+                <SparklesCore
+                    id="tsparticlesfullpage"
+                    background="transparent"
+                    minSize={0.6}
+                    maxSize={1.4}
+                    particleDensity={100}
+                    className="w-full h-full"
+                    particleColor="#FFFFFF"
+                />
             </div>
         </section>
     );
