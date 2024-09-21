d97570c53b3b4e2155d144a35a448e620222d859
d-randoll <51640895+d-randoll@users.noreply.github.com>
2024-09-21 15:38:12 -0400

    using next image

--- src/app/(root)/frame/page.tsx
+++ src/app/(root)/frame/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const HelloAmisha = () => {
@@ -5,7 +6,13 @@ const HelloAmisha = () => {
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
             <div className="bg-white p-8 rounded-lg shadow-xl text-center border-4 border-gray-300">
                 <div className="relative inline-block">
-                    <img src="/bubu-dudu.jpg" alt="Cute cats" className="mx-auto mb-4 w-36 h-36 rounded-full border-4 border-yellow-500" />
+                    <Image
+                        src="/bubu-dudu.jpg"
+                        alt="Cute cats"
+                        className="mx-auto mb-4 w-36 h-36 rounded-full border-4 border-yellow-500"
+                        width={100}
+                        height={100}
+                    />
                 </div>
                 <div className="text-3xl font-bold text-gray-900 italic font-sans">David & Amisha</div>
             </div>
